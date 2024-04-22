035661f1141669e2a6d8298d6126de7ce2906cf4
Layane Dantas Formiga <layanedantasformiga@gmail.com>
2024-04-22 15:47:22 -0300

    Conversor de Moedas Simples (Dolar para Real)

---
+++ ConversorMoeda/Conversor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoeda
+{
+    internal class Conversor
+    {
+
+
+        public static double Iof = 6.0;
+
+
+        public static double DolarParaReal(double cotacao, double quantia)
+        {
+            double total = cotacao * quantia;
+            return total + total * Iof / 100.0;
+
+        }
+
+
+
+        //Como eu havia feito antes da correção;
+
+        //public static double Iof = 6.0;
+
+
+        //public static double CotacaDollar(double cotacao)
+        //{
+        //    return cotacao;
+        //}
+
+        //public static double QuantidadeDollarCompra(double quantidade) 
+        //{
+
+        //    return quantidade;
+
+        //}
+
+
+        //public static double TotalEmReais(double total)
+        //{
+        //    return total + total* Iof/ 100 ;
+
+        //}
+
+    }
+}
---
+++ ConversorMoeda/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConversorMoeda;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+
+
+
+Console.WriteLine("Qual é a cotação do dólar? ");
+double dolar = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quantos dólares deseja comprar? ");
+double qte = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double result = Conversor.DolarParaReal(qte, dolar);
+
+Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+
+//Como estava antes de correção
+
+//Console.WriteLine("Qual é a cotação do dólar? ");
+//double dolar = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//Conversor.CotacaDollar(dolar);
+//Console.WriteLine("Quantos dólares deseja comprar? ");
+//double qte = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//Conversor.QuantidadeDollarCompra(qte);
+
+//double total = dolar * qte;
+
+//Console.WriteLine("Valor a ser pago em reais: " + Conversor.TotalEmReais(total).ToString("F2", CultureInfo.InvariantCulture));
+
+
